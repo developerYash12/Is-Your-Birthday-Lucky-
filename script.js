8e6bba1313fdf37cb89d6471364cc4b0a7c12430
Yash Shrivastava <100514707+developerYash12@users.noreply.github.com>
2022-10-14 01:44:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum,luckyNumber) {
+    if(sum%luckyNumber === 0){
+        outputBox.innerText = "Your Birthday is Lucky";
+    }else{
+        outputBox.innerText = "Not Lucky";
+    }
+}
+
+function checkDobIsLucky() {
+    const dob = dateOfBirth.value;
+    const  sum = calculateSum(dob);
+    if(sum&&dob)
+    compareValues(sum,luckyNumber.value)
+    else 
+    outputBox.innerText = "Please enter all fields";
+}
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i=0;i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click', checkDobIsLucky);
